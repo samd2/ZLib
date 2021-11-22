11422ba302f05ea1da785448a9ec12418c8314c2
sdarwin <samuel.d.darwin@gmail.com>
2021-11-22 16:18:45 -0600

    test

--- .drone/drone.bat
+++ .drone/drone.bat
@@ -21,7 +21,8 @@ call ci\common_install.bat
 
 echo '==================================> COMPILE'
 
-set B2_TARGETS=libs/!SELF!/test libs/!SELF!/example
+REM set B2_TARGETS=libs/!SELF!/test libs/!SELF!/example
+set B2_TARGETS=libs/!SELF!/test
 call !BOOST_ROOT!\libs\!SELF!\ci\build.bat
 
 ) else if "%DRONE_JOB_BUILDTYPE%" == "standalone-windows" (
